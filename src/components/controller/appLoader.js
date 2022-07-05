e9f71bef8399e5a653fd51395784a496927249ca
Blick <45530302+Blick7@users.noreply.github.com>
2022-07-05 13:01:04 +0300

    feat: setup everything for typescript(webpack, eslint)

--- src/components/controller/appLoader.js
+++ src/components/controller/appLoader.js
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
-            apiKey: '', // получите свой ключ https://newsapi.org/
+            apiKey: '74dd3b3fa8ab43e6a92e9d8bee1b0a83', // get api key https://newsapi.org/
         });
     }
 }
