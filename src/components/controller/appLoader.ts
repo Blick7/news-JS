9992e9afff5b6e1d7e9a3a9a006e192046d8ab9f
Blick <45530302+Blick7@users.noreply.github.com>
2022-07-07 15:09:55 +0300

    feat: add deploy url herokuapp

--- src/components/controller/appLoader.ts
+++ src/components/controller/appLoader.ts
@@ -2,7 +2,7 @@ import Loader from './loader';
 
 class AppLoader extends Loader {
     constructor() {
-        super('https://newsapi.org/v2/', {
+        super('https://nodenews.herokuapp.com/', {
             apiKey: '74dd3b3fa8ab43e6a92e9d8bee1b0a83', // get api key https://newsapi.org/
         });
     }
